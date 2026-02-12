e59a8919011a84517ea5b4136015a06431df4e41
VOICU <david-stefan.voicu@etu.univ-lyon1.fr>
2026-02-12 15:24:10 +0100

    red case on click

--- src/VueController/Board.java
+++ src/VueController/Board.java
@@ -2,15 +2,13 @@ package VueController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.*;
 
 public class Board {
     JPanel[][] tab = new JPanel[8][8];
 
     public Board(Window chessWindow){
-
-        // JPanel of the whole Window
-        JPanel jp = new JPanel();
-        jp.setLayout(new BorderLayout());
 
         //Jpanel of  the chess board
         JPanel pi = new JPanel(new GridLayout(8, 8));
@@ -18,7 +16,7 @@ public class Board {
         //Adding the board cases
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                tab[i][j] = new JPanel();
+                JPanel square = new JPanel();
 
                 if (i % 2 == 0) {
                     if (j % 2 == 0) {
@@ -29,11 +27,19 @@ public class Board {
                         tab[i][j].setBackground(Color.BLACK);
                     }
                 }
+                //Listener on each board case
+                square.addMouseListener(new MouseAdapter(){
+                    public void mouseClicked(MouseEvent e){
+                        square.setBackground(Color.RED);
+                    }
+                });
 
+                tab[i][j] = square;
                 pi.add(tab[i][j]);
             }
         }
-        jp.add(pi);
-        chessWindow.add(jp);
+        chessWindow.add(pi);
     }
 }
+
+
