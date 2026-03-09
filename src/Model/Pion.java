3624fa986a0d87ecf4c3f6637fef68b412212479
VOICU <david-stefan.voicu@etu.univ-lyon1.fr>
2026-03-09 13:54:10 +0100

    refacto: add image to pion class

--- src/Model/Pion.java
+++ src/Model/Pion.java
@@ -6,10 +6,12 @@ public class Pion extends Piece {
 
     private int ligne;
     private int colonne;
+    private String image;
 
-    public Pion(int ligne,int colonne) {
+    public Pion(int ligne,int colonne, String image) {
         this.ligne = ligne;
         this.colonne = colonne;
+        this.image = image;
     }
 
     @Override
