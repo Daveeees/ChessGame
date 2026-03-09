9a41438728fc150afd7008709cf52d5d3853689e
VOICU <david-stefan.voicu@etu.univ-lyon1.fr>
2026-03-09 14:40:18 +0100

    refacto: init board in model

--- src/Model/Jeu.java
+++ src/Model/Jeu.java
@@ -31,111 +31,70 @@ public class Jeu extends Observable{
         for(int i=0;i<=8;i++){
             for(int j=0;j<=8;j++){
                 if(i == 1){
-                    board[i][j] = new Case(i,j,"");
+                    Pion pN = new Pion(i,j,"chessPieces/bP.svg");
+                    board[i][j] = new Case(i,j, pN);
                 }
                 // autre pieces
-                else if(c.getLigne() == 0){
-                    if(c.getColonne() == 0 || c.getColonne() == 7){
-                        piece = "tour noire";
+                else if(i == 0){
+                    if(j == 0 || j == 7){
+                        Tour tN = new Tour(i,j, "chessPieces/bR.svg");
+                        board[i][j] = new Case(i,j, tN);
                     }
-                    else if(c.getColonne() == 1 || c.getColonne() == 6){
-                        piece = "cavalier noir";
+                    else if(j == 1 || j == 6){
+                        Cavalier cN = new Cavalier(i,j, "chessPieces/bN.svg");
+                        board[i][j] = new Case(i,j, cN);
                     }
-                    else if(c.getColonne() == 2 || c.getColonne() == 5){
-                        piece = "fou noir";
+                    else if(j == 2 || j == 5){
+                        Fou fN = new Fou(i,j, "chessPieces/bB.svg");
+                        board[i][j] = new Case(i,j,fN);
                     }
-                    else if(c.getColonne() == 3){
-                        piece = "reine noire";
+                    else if(j == 3){
+                        Reine reineN = new Reine(i,j, "chessPieces/bQ.svg");
+                        board[i][j] = new Case(i,j,reineN);
                     }
-                    else if(c.getColonne() == 4){
-                        piece = "roi noir";
+                    else if(j == 4){
+                        Roi roiN = new Roi(i,j, "chessPieces/bK.svg");
+                        board[i][j] = new Case(i,j,roiN);
                     }
                 }
 
                 // blancs
                 // pions
-                else if(c.getLigne() == 6){
-                    piece = "pion blanc";
+                else if(i == 6){
+                    Pion pB = new Pion(i,j,"chessPieces/wP.svg");
+                    board[i][j] = new Case(i,j, pB);
                 }
                 // autres pieces
-                else if (c.getLigne() == 7){
-                    if(c.getColonne() == 0 || c.getColonne() == 7){
-                        piece = "tour blanche";
+                else if (i == 7){
+                    if(j == 0 || j == 7){
+                        Tour tB = new Tour(i,j, "chessPieces/wR.svg");
+                        board[i][j] = new Case(i,j, tB);
                     }
-                    else if(c.getColonne() == 1 || c.getColonne() == 6){
-                        piece = "cavalier blanc";
+                    else if(j == 1 || j == 6){
+                        Cavalier cB = new Cavalier(i,j, "chessPieces/wN.svg");
+                        board[i][j] = new Case(i,j, cB);
                     }
-                    else if(c.getColonne() == 2 || c.getColonne() == 5){
-                        piece = "fou blanc";
+                    else if(j == 2 || j == 5){
+                        Fou fB = new Fou(i,j, "chessPieces/wB.svg");
+                        board[i][j] = new Case(i,j,fB);
                     }
-                    else if(c.getColonne() == 3){
-                        piece = "reine blanche";
+                    else if(j == 3){
+                        Reine reineB = new Reine(i,j, "chessPieces/wQ.svg");
+                        board[i][j] = new Case(i,j,reineB);
                     }
-                    else if(c.getColonne() == 4){
-                        piece = "roi blanc";
+                    else if(j == 4){
+                        Roi roiB = new Roi(i,j, "chessPieces/wK.svg");
+                        board[i][j] = new Case(i,j,roiB);
                     }
                 }
+
+                // cases vides
                 else{
-                    piece = "vide";
+                    board[i][j] = new Case(i,j,null);
                 }
             }
         }
-    }
-
-    public void initPiece(Case c){
-
-        // noirs
-
-        // pions
-        if(c.getLigne() == 1){
-            piece = "pion noir";
-        }
-        // autre pieces
-        else if(c.getLigne() == 0){
-            if(c.getColonne() == 0 || c.getColonne() == 7){
-                piece = "tour noire";
-            }
-            else if(c.getColonne() == 1 || c.getColonne() == 6){
-                piece = "cavalier noir";
-            }
-            else if(c.getColonne() == 2 || c.getColonne() == 5){
-                piece = "fou noir";
-            }
-            else if(c.getColonne() == 3){
-                piece = "reine noire";
-            }
-            else if(c.getColonne() == 4){
-                piece = "roi noir";
-            }
-        }
-
-        // blancs
-        // pions
-        else if(c.getLigne() == 6){
-            piece = "pion blanc";
-        }
-        // autres pieces
-        else if (c.getLigne() == 7){
-            if(c.getColonne() == 0 || c.getColonne() == 7){
-                piece = "tour blanche";
-            }
-            else if(c.getColonne() == 1 || c.getColonne() == 6){
-                piece = "cavalier blanc";
-            }
-            else if(c.getColonne() == 2 || c.getColonne() == 5){
-                piece = "fou blanc";
-            }
-            else if(c.getColonne() == 3){
-                piece = "reine blanche";
-            }
-            else if(c.getColonne() == 4){
-                piece = "roi blanc";
-            }
-        }
-        else{
-            piece = "vide";
-        }
-
     }
 
 }
+
